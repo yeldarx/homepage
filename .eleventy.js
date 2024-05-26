9beffe770b155126086ee6bc9357dd3c8067126f
Yeldar Kudaibergenov <mail@yeldar.org>
2024-05-27 00:03:59 +0500

    make width - 100% for every embed video

--- .eleventy.js
+++ .eleventy.js
@@ -17,12 +17,6 @@ module.exports = function(eleventyConfig) {
         return collectionApi.getFilteredByTag("blog").sort((a, b) => b.date - a.date);
       });
 
-    eleventyConfig.addFilter("embedYouTubeVideos", function(value) {
-        const youtubeLink = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/g;
-        const newValue = value.replace(youtubeLink, '<iframe width="560" height="315" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>');
-        return newValue;
-      });
-
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addFilter("postDate", (dateObj) => {
