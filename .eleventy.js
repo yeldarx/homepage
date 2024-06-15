647844a7263be0bc2d146eea1c2f1f8172bff286
Yeldar Kudaibergenov <mail@yeldar.org>
2024-06-15 22:23:37 +0500

    add rss icon

--- .eleventy.js
+++ .eleventy.js
@@ -25,6 +25,7 @@ module.exports = function(eleventyConfig) {
     
     eleventyConfig.addPassthroughCopy("src/assets/main.css");
     eleventyConfig.addPassthroughCopy("src/assets/img/favicon.ico");
+    eleventyConfig.addPassthroughCopy("src/assets/img/rss-icon.svg"); // rss icon
 
     return {
         dir: {
